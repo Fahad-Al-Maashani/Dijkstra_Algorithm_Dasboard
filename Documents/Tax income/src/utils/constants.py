30d5d98676ebbc5c3e76b482d903eb9587ea4137
Your Name <your.email@example.com>
2025-09-28 18:20:55 +0400

    Add application constants

---
+++ Documents/Tax income/src/utils/constants.py
@@ -0,0 +1,22 @@
+"""Application constants"""
+
+# Application info
+APP_NAME = 'Oman Tax Calculator'
+APP_VERSION = '1.0.0'
+APP_AUTHOR = 'Tax Calculator Team'
+
+# Tax constants
+TAX_THRESHOLD_OMR = 42000.0
+TAX_RATE_PERCENT = 5.0
+EFFECTIVE_YEAR = 2028
+
+# UI constants
+WINDOW_MIN_WIDTH = 600
+WINDOW_MIN_HEIGHT = 500
+DEFAULT_WINDOW_WIDTH = 800
+DEFAULT_WINDOW_HEIGHT = 600
+
+# File constants
+CONFIG_DIR = '.oman_tax_calculator'
+LOG_DIR = 'logs'
+BACKUP_DIR = 'backups'
